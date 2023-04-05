1996d88c30547a543fac46b1435432add0b99835
Stroescu Stefan <stroescutudor@yahoo.com>
2023-04-05 08:21:09 +0300

    Task A solved
    Signed-off-by: Stroescu Stefan <stroescutudor@yahoo.com>

---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Colocviu1_1MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.colocviu1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu1_1MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_1_main);
+    }
+}
